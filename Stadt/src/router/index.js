46b9d84ede8b0a6a641a9910e29e85c482700d46
ArmitaAkhavanJorshari <armita.akhavan-jorshari@stud.hs-bochum.de>
2026-06-10 16:49:05 +0200

    verbesserungen1

--- Stadt/src/router/index.js
+++ Stadt/src/router/index.js
@@ -8,22 +8,10 @@ import MarketplaceView from '../views/marketplaceview.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/',
-      component: UniView
-    },
-    {
-      path: '/events',
-      component: EventView
-    },
-    {
-      path: '/erste-schritte',
-      component: ErsteSchrittView
-    },
-    {
-      path: '/Marketplace',
-      component: MarketplaceView
-    }
+    {path: '/', component: UniView},
+    {path: '/events',component: EventView},
+    {path: '/erste-schritte',component: ErsteSchrittView},
+    {path: '/Marketplace',component: MarketplaceView}
   ]
 })
 

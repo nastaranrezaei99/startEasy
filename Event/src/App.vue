6f87b3e2cbcd8287769bd7225bafe90212a79a1f
Nastaran Rezaei <nastaran.rezaei@stud.hs-bochum.de>
2026-06-30 10:37:40 +0200

    evet-backend-vue-verbindung

--- Event/src/App.vue
+++ Event/src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
+import axios from 'axios'
 import EventCard from './components/EventCard.vue'
 import EventForm from './components/EventForm.vue'
 import SignupBox from './components/SignupBox.vue'
@@ -7,32 +8,12 @@ import SignupBox from './components/SignupBox.vue'
 const showCreateForm = ref(false)
 const showSignup = ref(false)
 
-const events = ref([
-  {
-    id: 1,
-    title: 'Lerngruppe Informatik',
-    date: '20.05.2026',
-    time: '12:00',
-    location: 'RUB Bibliothek',
-    participants: 5
-  },
-  {
-    id: 2,
-    title: 'Fußball spielen',
-    date: '23.05.2026',
-    time: '17:00',
-    location: 'Querenburger Straße 29, 44789 Bochum',
-    participants: 3
-  },
-  {
-    id: 3,
-    title: 'Coffee Meetup',
-    date: '10.05.2026',
-    time: '19:00',
-    location: 'Bermudadreieck',
-    participants: 5
-  }
-])
+const events = ref([])
+
+async function loadEvents() {
+  const response = await axios.get('http://127.0.0.1:8000/api/events')
+  events.value = response.data
+}
 
 function openCreateForm() {
   showCreateForm.value = true
@@ -42,6 +23,27 @@ function openCreateForm() {
 function openSignup() {
   showSignup.value = true
 }
+
+function closeSignup() {
+  showSignup.value = false
+}
+
+async function addEvent(newEvent) {
+  const response = await axios.post('http://127.0.0.1:8000/api/events', newEvent)
+  events.value.push(response.data)
+  showSignup.value = true
+}
+
+async function joinEvent(id) {
+  const response = await axios.post(`http://127.0.0.1:8000/api/events/${id}/join`)
+
+  const index = events.value.findIndex(event => event.id === id)
+  events.value[index] = response.data
+}
+
+onMounted(() => {
+  loadEvents()
+})
 </script>
 
 <template>
@@ -61,16 +63,17 @@ function openSignup() {
 
     <section class="events">
       <EventCard
-        v-for="event in events"
-        :key="event.id"
-        :event="event"
-      />
+  v-for="event in events"
+  :key="event.id"
+  :event="event"
+  @join="joinEvent"
+/>
     </section>
 
     <section v-if="showCreateForm" class="create-area">
-      <EventForm @formSubmit="openSignup" />
+      <EventForm @formSubmit="addEvent" />
 
-      <SignupBox v-if="showSignup" />
+<SignupBox v-if="showSignup" @login-success="closeSignup" />
     </section>
   </main>
 </template>
